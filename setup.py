d1fd4be49e9e035d6b2e7088fb5526e1df81ebca
Brian Jorgensen <brian.jorgensen@gmail.com>
2015-06-06 13:45:49 -0700

    Added setup.cfg, fixup setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup
 
+PROJECT_URL = 'https://github.com/b33j0r/climactic'
+PROJECT_VERSION = '0.1.0'
+
 setup(
     name='climactic',
-    version='0.1',
+    version=PROJECT_VERSION,
     packages=[
-        'climactic',
-        'climactic.test'
+        'climactic'
     ],
-    url='http://www.github.com',
-    license='GPLv3',
+    url=PROJECT_URL,
+    download_url=PROJECT_URL + '/tarball/' + PROJECT_VERSION,
+    license='GPLv2',
     author='Brian Jorgensen',
     author_email='brian.jorgensen@gmail.com',
     description='YAML-based test framework for '
-                'command-line utilities'
+                'command-line utilities',
+    keywords=['testing', 'cli', 'yaml', 'unittest']
 )
